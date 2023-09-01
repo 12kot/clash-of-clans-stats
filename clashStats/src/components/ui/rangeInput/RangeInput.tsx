585959ce63e59ea0113b7921b304822785edaddf
Nikita Yakoltsevich <Art.artem.nik@gmail.com>
2023-09-01 23:53:01 +0300

    new design

---
+++ clashStats/src/components/ui/rangeInput/RangeInput.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./RangeInput.scss";
+
+const RangeInput = () => {
+  return (
+    <div className="slider">
+      
+
+      <div className="values">
+        <p className="first">1</p>
+        <p className="second">10</p>
+      </div>
+    </div>
+  );
+};
+
+export default RangeInput;
--- clashStats/src/pages/search/clanDetails/detailsClanList/DetailsClanList.tsx
+++ clashStats/src/pages/search/clanDetails/detailsClanList/DetailsClanList.tsx
@@ -1,51 +1,77 @@
 import React from "react";
 import styles from "./DetailsClanList.module.scss";
-import im from "assets/svg/3.png";
+import im1 from "assets/svg/1.png";
+import TT from "assets/svg/trophy.png";
+import versus from "assets/svg/versus.png";
+import exp from "assets/svg/exp.png";
+import vv from "assets/svg/1v.png";
+import vv10 from "assets/svg/10v.png";
+import RangeInput from "components/ui/rangeInput/RangeInput";
 
 const DetailsClanList = () => {
   return (
-    <ul className={styles.details}>
-      <li className={styles.detailsItem}>
-        <span className={styles.radioInput}>
-          <img src={im} alt=""></img>
-          <input type="range" max="100"></input>
-          <img src={im} alt=""></img>
-        </span>
-        <label className={styles.label}>Required Townhall Level</label>
-      </li>
-      <li className={styles.detailsItem}>
-        <span className={styles.radioInput}>
-          <img src={im} alt=""></img>
-          <input type="range" max="100"></input>
-          <img src={im} alt=""></img>
-        </span>
-        <label className={styles.label}>Required Trophies</label>
-      </li>
-      <li className={styles.detailsItem}>
-        <span className={styles.radioInput}>
-          <img src={im} alt=""></img>
-          <input type="range" max="100"></input>
-          <img src={im} alt=""></img>
-        </span>
-        <label className={styles.label}>Required Versus Trophies</label>
-      </li>
-      <li className={styles.detailsItem}>
-        <span className={styles.radioInput}>
-          <img src={im} alt=""></img>
-          <input type="range" max="100"></input>
-          <img src={im} alt=""></img>
-        </span>
-        <label className={styles.label}>Clan Level</label>
-      </li>
-      <li className={styles.detailsItem}>
-        <span className={styles.radioInput}>
-          <img src={im} alt=""></img>
-          <input type="range" max="100"></input>
-          <img src={im} alt=""></img>
-        </span>
-        <label className={styles.label}>Clan Trophies</label>
-      </li>
-    </ul>
+    <div className={styles.container}>
+      <section className={styles.section}>
+        <h2>Required</h2>
+        <ul className={styles.details}>
+          <li className={styles.detailsItem}>
+            <img src={im1} alt=""></img>
+            <span className={styles.input}>
+              <RangeInput />
+            </span>
+          </li>
+          <li className={styles.detailsItem}>
+            <img src={TT} alt=""></img>
+            <span className={styles.input}>
+              <RangeInput />
+            </span>
+          </li>
+          <li className={styles.detailsItem}>
+            <img src={versus} alt=""></img>
+
+            <span className={styles.input}>
+              <RangeInput />
+            </span>
+          </li>
+        </ul>
+      </section>
+      <section className={styles.section}>
+        <h2>Details</h2>
+        <ul className={styles.details}>
+          <li className={styles.detailsItem}>
+            <img src={TT} alt=""></img>
+            <span className={styles.input}>
+              <RangeInput />
+            </span>
+          </li>
+          <li className={styles.detailsItem}>
+            <img src={versus} alt=""></img>
+            <span className={styles.input}>
+              <RangeInput />
+            </span>
+          </li>
+          <li className={styles.detailsItem}>
+            <img src={exp} alt=""></img>
+            <span className={styles.input}>
+              <RangeInput />
+            </span>
+          </li>
+          <li className={styles.detailsItem}>
+            <img src={vv} alt=""></img>
+            <span className={styles.input}>
+              <RangeInput />
+            </span>
+            <img src={vv10} alt=""></img>
+          </li>
+          <li className={styles.detailsItem}>
+            <img src={exp} alt=""></img>
+            <span className={styles.input}>
+              <RangeInput />
+            </span>
+          </li>
+        </ul>
+      </section>
+    </div>
   );
 };
 
