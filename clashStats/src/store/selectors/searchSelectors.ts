e2e881bd75e27f220653fa2b8394c94d9e9fbafa
Nikita Yakoltsevich <Art.artem.nik@gmail.com>
2023-09-13 22:47:51 +0300

    added sort in players

--- clashStats/src/store/selectors/searchSelectors.ts
+++ clashStats/src/store/selectors/searchSelectors.ts
@@ -1,6 +1,7 @@
 import { TStore } from "types/types/slices/storeType";
 
 export const selectSearchPlayers = (state: TStore) => state.search.players;
+export const selectSearchPlayersList = (state: TStore) => state.search.players.list;
 export const selectSearchPlayersLoading = (state: TStore) => state.search.players.loading;
 
 export const selectSearchClans = (state: TStore) => state.search.clans;
--- clashStats/src/store/selectors/sortSelectors.ts
+++ clashStats/src/store/selectors/sortSelectors.ts
@@ -1,57 +1,108 @@
 import { createSelector } from "@reduxjs/toolkit";
 import { TStore } from "types/types/slices/storeType";
-import { selectSearchClansList } from "./searchSelectors";
+import {
+  selectSearchClansList,
+  selectSearchPlayersList,
+} from "./searchSelectors";
 
 export const selectSortBasicClan = (state: TStore) => state.sort.basic.clan;
-export const selectDasicClanCurrentSort = (state: TStore) =>
+export const selectBasicClanCurrentSort = (state: TStore) =>
   state.sort.basic.clan.current;
 
 export const selectSortBasicPlayer = (state: TStore) => state.sort.basic.player;
+export const selectBasicPlayerCurrentSort = (state: TStore) =>
+  state.sort.basic.player.current;
 
 export const selectBasicCardsClanByFilter = createSelector(
-  [selectDasicClanCurrentSort, selectSearchClansList],
+  [selectBasicClanCurrentSort, selectSearchClansList],
   (activeSort, clans) => {
     if (activeSort.key === "name")
-        return clans.slice().sort((a, b) => {
-          console.log(a.mainInfo.name, b.mainInfo.name)
-        return a.mainInfo.name.localeCompare(b.mainInfo.name)}
-      );
+      return clans
+        .slice()
+        .sort((a, b) => a.mainInfo.name.localeCompare(b.mainInfo.name));
 
     if (activeSort.key === "tag")
-      return clans.slice().sort((a, b) =>
-        a.mainInfo.tag.localeCompare(b.mainInfo.tag)
-      );
+      return clans
+        .slice()
+        .sort((a, b) => a.mainInfo.tag.localeCompare(b.mainInfo.tag));
 
     if (activeSort.key === "capitalLevel")
-      return clans.slice().sort(
-        (a, b) => a.mainInfo.capital.value - b.mainInfo.capital.value
-      );
+      return clans
+        .slice()
+        .sort((a, b) => a.mainInfo.capital.value - b.mainInfo.capital.value);
 
     if (activeSort.key === "level")
-      return clans.slice().sort(
-        (a, b) => a.otherInfo.level.value - b.otherInfo.level.value
-      );
+      return clans
+        .slice()
+        .sort((a, b) => a.otherInfo.level.value - b.otherInfo.level.value);
 
     if (activeSort.key === "members")
-      return clans.slice().sort(
-        (a, b) => a.otherInfo.members.value - b.otherInfo.members.value
-      );
+      return clans
+        .slice()
+        .sort((a, b) => a.otherInfo.members.value - b.otherInfo.members.value);
 
     if (activeSort.key === "trophies")
-      return clans.slice().sort(
-        (a, b) => a.trophies.main.value - b.trophies.main.value
-      );
+      return clans
+        .slice()
+        .sort((a, b) => a.trophies.main.value - b.trophies.main.value);
 
     if (activeSort.key === "versusTrophies")
-      return clans.slice().sort(
-        (a, b) => a.trophies?.versus.value - b.trophies.versus.value
-      );
+      return clans
+        .slice()
+        .sort((a, b) => a.trophies.versus.value - b.trophies.versus.value);
 
     if (activeSort.key === "capitalTrophies")
-      return clans.slice().sort(
-        (a, b) => a.trophies?.capital.value - b.trophies.capital.value
-      );
+      return clans
+        .slice()
+        .sort((a, b) => a.trophies.capital.value - b.trophies.capital.value);
 
     return clans;
+  }
+);
+
+export const selectBasicCardsPlayerByFilter = createSelector(
+  [selectBasicClanCurrentSort, selectSearchPlayersList],
+  (activeSort, players) => {
+    if (activeSort.key === "name")
+      return players
+        .slice()
+        .sort((a, b) => a.mainInfo.name.localeCompare(b.mainInfo.name));
+
+    if (activeSort.key === "tag")
+      return players
+        .slice()
+        .sort((a, b) => a.mainInfo.tag.localeCompare(b.mainInfo.tag));
+
+    if (activeSort.key === "level")
+      return players
+        .slice()
+        .sort((a, b) => a.otherInfo.level.value - b.otherInfo.level.value);
+
+    if (activeSort.key === "stars")
+      return players
+        .slice()
+        .sort((a, b) => a.otherInfo.stars.value - b.otherInfo.stars.value);
+
+    if (activeSort.key === "townhall")
+      return players
+        .slice()
+        .sort((a, b) => a.mainInfo.townHall.value - b.mainInfo.townHall.value);
+
+    if (activeSort.key === "trophies")
+      return players
+        .slice()
+        .sort((a, b) => a.trophies.main.value - b.trophies.main.value);
+
+    if (activeSort.key === "versusTrophies")
+      return players
+        .slice()
+        .sort((a, b) => a.trophies.versus.value - b.trophies.versus.value);
+
+    if (activeSort.key === "mainLegend")
+      return players
+        .slice()
+        .sort((a, b) => a.trophies.mainLegend.value - b.trophies.mainLegend.value);
+
+    return players;
   }
 );
