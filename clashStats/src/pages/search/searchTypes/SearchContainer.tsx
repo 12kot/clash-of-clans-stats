f92619b2b4d8eb23763838b8dfa821cf261f2f07
Nikita Yakoltsevich <Art.artem.nik@gmail.com>
2023-09-18 13:02:45 +0300

    flex grid to grid

--- clashStats/src/pages/search/searchTypes/SearchContainer.tsx
+++ clashStats/src/pages/search/searchTypes/SearchContainer.tsx
@@ -2,6 +2,8 @@ import React, { ReactElement } from "react";
 import styles from "./SearchTypes.module.scss";
 import CardSettings from "components/card/settings/CardSetting";
 import { TSortTypes } from "types/types/card/basic/sort/sortBasicTypes";
+import { useAppSelector } from "hooks/store";
+import { selectViewType } from "store/selectors/viewTypeSelectors";
 
 interface Props extends TSortTypes {
   Filters: ReactElement;
@@ -9,12 +11,14 @@ interface Props extends TSortTypes {
 }
 
 const SearchContainer = ({ Filters, cards, type }: Props) => {
+  const viewType = useAppSelector(selectViewType); 
+
   return (
     <main className={styles.container}>
       {Filters}
       <CardSettings type={type} />
 
-      <article className={`${styles.cards} ${styles.cardsLine}`}>
+      <article className={`${styles.cards} ${viewType === "block" && styles.cardsBlock}`}>
         {cards}
       </article>
     </main>
