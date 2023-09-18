91197927fae10ae535d88ad926ef83962d7e3572
Nikita Yakoltsevich <Art.artem.nik@gmail.com>
2023-09-18 12:36:00 +0300

    added sort direction logic

--- clashStats/src/store/selectors/sortSelectors.ts
+++ clashStats/src/store/selectors/sortSelectors.ts
@@ -15,104 +15,192 @@ export const selectBasicPlayerCurrentSort = (state: TStore) =>
 
 export const selectSortDirection = (state: TStore) => state.sort.direction;
 
-const compareString = (a: string, b: string): number => {
-  return a.localeCompare(b);
+const compareString = (
+  a: string,
+  b: string,
+  direction: "more" | "less"
+): number => {
+  if (direction === "more") return a.localeCompare(b);
+  return b.localeCompare(a);
 };
 
-const compareNumbers = (a: number, b: number): number => {
-  return a - b;
+const compareNumbers = (
+  a: number,
+  b: number,
+  direction: "more" | "less"
+): number => {
+  if (direction === "more") return a - b;
+  return b - a;
 };
 
 export const selectBasicCardsClanByFilter = createSelector(
-  [selectBasicClanCurrentSort, selectSearchClansList],
-  (activeSort, clans) => {
+  [selectBasicClanCurrentSort, selectSearchClansList, selectSortDirection],
+  (activeSort, clans, sortDirection) => {
     if (activeSort.key === "name")
       return clans
         .slice()
-        .sort((a, b) => compareString(a.mainInfo.name, b.mainInfo.name));
+        .sort((a, b) =>
+          compareString(a.mainInfo.name, b.mainInfo.name, sortDirection)
+        );
 
     if (activeSort.key === "tag")
       return clans
         .slice()
-        .sort((a, b) => compareString(a.mainInfo.tag, b.mainInfo.tag));
+        .sort((a, b) =>
+          compareString(a.mainInfo.tag, b.mainInfo.tag, sortDirection)
+        );
 
     if (activeSort.key === "capitalLevel")
       return clans
         .slice()
-        .sort((a, b) => compareNumbers(a.mainInfo.capital.value, b.mainInfo.capital.value));
+        .sort((a, b) =>
+          compareNumbers(
+            a.mainInfo.capital.value,
+            b.mainInfo.capital.value,
+            sortDirection
+          )
+        );
 
     if (activeSort.key === "level")
       return clans
         .slice()
-        .sort((a, b) => compareNumbers(a.otherInfo.level.value, b.otherInfo.level.value));
+        .sort((a, b) =>
+          compareNumbers(
+            a.otherInfo.level.value,
+            b.otherInfo.level.value,
+            sortDirection
+          )
+        );
 
     if (activeSort.key === "members")
       return clans
         .slice()
-        .sort((a, b) => compareNumbers(a.otherInfo.members.value, b.otherInfo.members.value));
+        .sort((a, b) =>
+          compareNumbers(
+            a.otherInfo.members.value,
+            b.otherInfo.members.value,
+            sortDirection
+          )
+        );
 
     if (activeSort.key === "trophies")
       return clans
         .slice()
-        .sort((a, b) => compareNumbers(a.trophies.main.value, b.trophies.main.value));
+        .sort((a, b) =>
+          compareNumbers(
+            a.trophies.main.value,
+            b.trophies.main.value,
+            sortDirection
+          )
+        );
 
     if (activeSort.key === "versusTrophies")
       return clans
         .slice()
-        .sort((a, b) => compareNumbers(a.trophies.versus.value, b.trophies.versus.value));
+        .sort((a, b) =>
+          compareNumbers(
+            a.trophies.versus.value,
+            b.trophies.versus.value,
+            sortDirection
+          )
+        );
 
     if (activeSort.key === "capitalTrophies")
       return clans
         .slice()
-        .sort((a, b) => compareNumbers(a.trophies.capital.value, b.trophies.capital.value));
+        .sort((a, b) =>
+          compareNumbers(
+            a.trophies.capital.value,
+            b.trophies.capital.value,
+            sortDirection
+          )
+        );
 
     return clans;
   }
 );
 
 export const selectBasicCardsPlayerByFilter = createSelector(
-  [selectBasicClanCurrentSort, selectSearchPlayersList],
-  (activeSort, players) => {
+  [selectBasicClanCurrentSort, selectSearchPlayersList, selectSortDirection],
+  (activeSort, players, sortDirection) => {
     if (activeSort.key === "name")
       return players
         .slice()
-        .sort((a, b) => a.mainInfo.name.localeCompare(b.mainInfo.name));
+        .sort((a, b) =>
+          compareString(a.mainInfo.name, b.mainInfo.name, sortDirection)
+        );
 
     if (activeSort.key === "tag")
       return players
         .slice()
-        .sort((a, b) => a.mainInfo.tag.localeCompare(b.mainInfo.tag));
+        .sort((a, b) =>
+          compareString(a.mainInfo.tag, b.mainInfo.tag, sortDirection)
+        );
 
     if (activeSort.key === "level")
       return players
         .slice()
-        .sort((a, b) => a.otherInfo.level.value - b.otherInfo.level.value);
+        .sort((a, b) =>
+          compareNumbers(
+            a.otherInfo.level.value,
+            b.otherInfo.level.value,
+            sortDirection
+          )
+        );
 
     if (activeSort.key === "stars")
       return players
         .slice()
-        .sort((a, b) => a.otherInfo.stars.value - b.otherInfo.stars.value);
+        .sort((a, b) =>
+          compareNumbers(
+            a.otherInfo.stars.value,
+            b.otherInfo.stars.value,
+            sortDirection
+          )
+        );
 
     if (activeSort.key === "townhall")
       return players
         .slice()
-        .sort((a, b) => a.mainInfo.townHall.value - b.mainInfo.townHall.value);
+        .sort((a, b) =>
+          compareNumbers(
+            a.mainInfo.townHall.value,
+            b.mainInfo.townHall.value,
+            sortDirection
+          )
+        );
 
     if (activeSort.key === "trophies")
       return players
         .slice()
-        .sort((a, b) => a.trophies.main.value - b.trophies.main.value);
+        .sort((a, b) =>
+          compareNumbers(
+            a.trophies.main.value,
+            b.trophies.main.value,
+            sortDirection
+          )
+        );
 
     if (activeSort.key === "versusTrophies")
       return players
         .slice()
-        .sort((a, b) => a.trophies.versus.value - b.trophies.versus.value);
+        .sort((a, b) =>
+          compareNumbers(
+            a.trophies.versus.value,
+            b.trophies.versus.value,
+            sortDirection
+          )
+        );
 
     if (activeSort.key === "mainLegend")
       return players
         .slice()
-        .sort(
-          (a, b) => a.trophies.mainLegend.value - b.trophies.mainLegend.value
+        .sort((a, b) =>
+          compareNumbers(
+            a.trophies.mainLegend.value,
+            b.trophies.mainLegend.value,
+            sortDirection
+          )
         );
 
     return players;
